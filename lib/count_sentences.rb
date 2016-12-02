cb31711e183e325c37314e9314474d59090ccc05
limerentfox <christi@limerentfox.com>
2016-12-02 16:02:48 -0500

    fixed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,30 +1,27 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    return true if self[-1] == "."
-    false
-  end
-
-  def question?
-    return true if self[-1] == "?"
-    false
-  end
-
-  def exclamation?
-    return true if self[-1] == "!"
-    false
-  end
-
-  def count_sentences
-    count = 0
-    self.strip.split(' ').each do |word|
-    if word[-1] == '.' || word[-1] == '?' || word[-1] == '!'
-      count += 1
+    def sentence?
+        return true if self[-1] == '.'
+        false
     end
-  end
 
-    return count
-  end
+    def question?
+        return true if self[-1] == '?'
+        false
+    end
+
+    def exclamation?
+        return true if self[-1] == '!'
+        false
+    end
+
+    def count_sentences
+        count = 0
+        strip.split(' ').each do |word|
+            count += 1 if word[-1] == '.' || word[-1] == '?' || word[-1] == '!'
+        end
+
+        count
+    end
 end
